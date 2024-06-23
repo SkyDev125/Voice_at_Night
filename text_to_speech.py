0da0a15b53025d710a896ffefc3e3aba681c0806
Diogo Santos <diogo_goncalves2004@hotmail.com>
2024-06-23 10:28:39 +0100

    Reduced tts complexity

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,37 +1,40 @@
 import gtts
-import pygame
-
-from time import sleep
 from pydub import AudioSegment
-from pygame import mixer, _sdl2 as devices
+import sounddevice as sd
+import numpy as np
+import io
 
-def play(file_path: str, device: str):
-    print("Play: {}\r\nDevice: {}".format(file_path, device))
-    pygame.mixer.init(devicename=device)
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play()
-    duration = get_audio_duration(file_path)
-    sleep(duration)
-    pygame.mixer.quit()
+def play(audio_data, device_id):
+    audio = AudioSegment.from_file(io.BytesIO(audio_data), format="mp3")
+    samples = np.array(audio.get_array_of_samples())
+    if audio.channels == 2:
+        samples = samples.reshape((-1, 2))
+    fs = audio.frame_rate
 
-def text_to_speech(text):
+    if device_id is not None:
+        sd.play(samples, samplerate=fs, device=device_id)
+        sd.wait()  # Wait until the file is fully played
+    else:
+        raise ValueError(f"Device '{device_id}' not found.")
+
+def text_to_speech(text: str) -> bytes:
     tts = gtts.gTTS(text, lang="en")
-    tts.save('temp.mp3')
-
-def get_audio_duration(file_path):
-    audio = AudioSegment.from_file(file_path)
-    duration_in_seconds = len(audio) / 1000  # Converting milliseconds to seconds
-    return duration_in_seconds
+    buf = io.BytesIO()
+    tts.write_to_fp(buf)
+    buf.seek(0)  # Rewind the buffer to the beginning
+    return buf.read()
 
 def get_audio_input_device():
-    # Initializing mixer
-    mixer.init()  
+    # This function will now list output devices and return the name of the first one
+    devices = sd.query_devices()
+    output_devices = [d for d in devices if d['max_output_channels'] > 0]
+    if output_devices:
+        # Find the device ID for the given device name
+        print("Device: {}".format(output_devices[8]['name']))
+        return output_devices[8]['index']
+    else:
+        raise ValueError("No output devices found")
 
-    # Selecting the specified device
-    audio_devices = devices.audio.get_audio_device_names(False)
-    selected_device = audio_devices[1]  
-
-    # Quitting mixer
-    mixer.quit()
-
-    return selected_device
+# Convert text to speech and play it directly from memory
+audio_data = text_to_speech("Hello, World!")
+play(audio_data, get_audio_input_device())
